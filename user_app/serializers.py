5b98950188ee5d1945e3b98f06119712a98233ac
gairik <gairikgmandal@gmail.com>
2025-06-10 15:24:04 +0530

    changes in userapp serjializers

--- user_app/serializers.py
+++ user_app/serializers.py
@@ -12,28 +12,59 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password', 'first_name', 'last_name','password2']
 
-    extra_kwargs = {
+        extra_kwargs = {
         'password': {'write_only': True, 'style': {'input_type': 'password'}},  
-    }
+      }
+        
+    def validate(self, data):
+        # Check if passwords match
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+
+        # Check if username already exists
+        if User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError({"username": "Username already exists."})
+
+        # Check if email already exists
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError({"email": "Email already exists."})
+
+        return data
     
-    def save(self):
+    
+    def create(self, validated_data):
+        # Remove password2 as it is not part of User model
+        validated_data.pop('password2')
+
         user = User(
-            username=self.validated_data['username'],
-            email=self.validated_data['email'],
-            first_name=self.validated_data['first_name'],
-            last_name=self.validated_data['last_name']
+            username=validated_data['username'],
+            email=validated_data['email'],
+            
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
-        password = self.validated_data['password']
-        password2 = self.validated_data['password2']
-        
-        if password != password2:
-            raise serializers.ValidationError("Passwords do not match.")
-        
-        if User.objects.filter(email=self.validated_data['email']).exists():
-            raise serializers.ValidationError("Email already exists.")
-        user.set_password(password)
+        user.set_password(validated_data['password'])
         user.save()
         return user
+    
+    # def save(self):
+    #     user = User(
+    #         username=self.validated_data['username'],
+    #         email=self.validated_data['email'],
+    #         first_name=self.validated_data['first_name'],
+    #         last_name=self.validated_data['last_name']
+    #     )
+    #     password = self.validated_data['password']
+    #     password2 = self.validated_data['password2']
+        
+    #     if password != password2:
+    #         raise serializers.ValidationError("Passwords do not match.")
+        
+    #     if User.objects.filter(email=self.validated_data['email']).exists():
+    #         raise serializers.ValidationError("Email already exists.")
+    #     user.set_password(password)
+    #     user.save()
+    #     return user
     
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
